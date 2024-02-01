a299a21b74ce69654a9bed360e8fe3d849c8c96b
ori-milstein <mydoal@keemail.me>
2024-02-01 17:06:11 +0200

    Refactor onBallclick to not use global vars

--- main.js
+++ main.js
@@ -1,12 +1,15 @@
 'use strict'
 
-var ballWidth = 100
-var ballHeight = 100
-
 function onBallClick(elBall) {
-    ballWidth += 50
-    ballHeight += 50
-    elBall.style.width = `${ballWidth}px`
-    elBall.style.height = `${ballHeight}px`
-    elBall.innerText = `${ballHeight}`
+    const newWidth = (elBall.innerText === '100') ?
+        +(elBall.style.width.split('p')[0]) + 150 + 'px' :
+        +(elBall.style.width.split('p')[0]) + 50 + 'px'
+    const newHeight = (elBall.innerText === '100') ?
+        +(elBall.style.height.split('p')[0]) + 150 + 'px' :
+        +(elBall.style.height.split('p')[0]) + 50 + 'px'
+    const newText = +elBall.innerText + 50 + ''
+
+    elBall.style.width = newWidth
+    elBall.style.height = newHeight
+    elBall.innerText = newText
 }
