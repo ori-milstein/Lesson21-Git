beccbefdf9473141814476f1cb9b1386297857b6
ori-milstein <mydoal@keemail.me>
2024-02-01 17:19:48 +0200

    Reset ball size to 100px if clicked after 400px

--- main.js
+++ main.js
@@ -1,13 +1,22 @@
 'use strict'
 
 function onBallClick(elBall) {
-    const newWidth = (elBall.innerText === '100') ?
-        +(elBall.style.width.split('p')[0]) + 150 + 'px' :
-        +(elBall.style.width.split('p')[0]) + 50 + 'px'
-    const newHeight = (elBall.innerText === '100') ?
-        +(elBall.style.height.split('p')[0]) + 150 + 'px' :
-        +(elBall.style.height.split('p')[0]) + 50 + 'px'
-    const newText = +elBall.innerText + 50 + ''
+    var newWidth
+    var newHeight
+    var newText
+    if (elBall.innerText === '400') {
+        newWidth = '100px'
+        newHeight = '100px'
+        newText = '100'
+    } else {
+        newWidth = (!elBall.style.width) ?
+            +(elBall.style.width.split('p')[0]) + 150 + 'px' :
+            +(elBall.style.width.split('p')[0]) + 50 + 'px'
+        newHeight = (!elBall.style.height) ?
+            +(elBall.style.height.split('p')[0]) + 150 + 'px' :
+            +(elBall.style.height.split('p')[0]) + 50 + 'px'
+        newText = +elBall.innerText + 50 + ''
+    }
 
     elBall.style.width = newWidth
     elBall.style.height = newHeight
