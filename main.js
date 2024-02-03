df81e924d66128946ef56c70efa842f704353221
ori-milstein <mydoal@keemail.me>
2024-02-03 19:50:08 +0200

    inc size by 20-60px &change color randly onClick

--- main.js
+++ main.js
@@ -4,21 +4,24 @@ function onBallClick(elBall) {
     var newWidth
     var newHeight
     var newText
-    if (elBall.innerText === '400') {
+    if (+elBall.innerText >= '400') {
         newWidth = '100px'
         newHeight = '100px'
         newText = '100'
     } else {
+        var randInc = getRandomInt(20, 61)
+
         newWidth = (!elBall.style.width) ?
-            +(elBall.style.width.split('p')[0]) + 150 + 'px' :
-            +(elBall.style.width.split('p')[0]) + 50 + 'px'
+            +(elBall.style.width.split('p')[0]) + 100 + randInc + 'px' :
+            +(elBall.style.width.split('p')[0]) + randInc + + 'px'
         newHeight = (!elBall.style.height) ?
-            +(elBall.style.height.split('p')[0]) + 150 + 'px' :
-            +(elBall.style.height.split('p')[0]) + 50 + 'px'
-        newText = +elBall.innerText + 50 + ''
+            +(elBall.style.height.split('p')[0]) + 100 + randInc + 'px' :
+            +(elBall.style.height.split('p')[0]) + randInc + + 'px'
+        newText = +elBall.innerText + randInc + ''
     }
 
     elBall.style.width = newWidth
     elBall.style.height = newHeight
     elBall.innerText = newText
+    elBall.style.backgroundColor = getRandomColor()
 }
